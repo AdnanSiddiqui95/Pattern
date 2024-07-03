e0c7ddc1f45603d07cbe188469ab14c78578bb33
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-03 17:33:33 +0530

    Add files via upload

---
+++ PatternOne8.java
@@ -0,0 +1,42 @@
+public class PatternOne8
+{
+	public static void main(String[] args) 
+	{
+		int n=5;
+		
+		for(int r=1;r<=n;r++)
+		{
+			for(int s=n-r;s>=1;s--)
+			{
+				System.out.print(" ");
+			}
+			for(int c=1;c<=r;c++)
+			{
+				System.out.print(c);
+			}
+			for(int c=r-1;c>=1;c--)
+			{
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+		for(int r=n-1;r>=1;r--)
+		{
+			for(int s=n-r;s>=1;s--)
+			{
+				System.out.print(" ");
+			}
+			for(int c=1;c<=r;c++)
+			{
+				System.out.print(c);
+			}
+			for(int c=r-1;c>=1;c--)
+			{
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
