5ae1d13771ce8ff760438988a23dafc07afb311e
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-04 17:28:28 +0530

    Add files via upload

---
+++ PatternCode13.java
@@ -0,0 +1,31 @@
+public class PatternCode13
+{
+   public static void main(String[] args)
+    {
+      int n=5;
+      for(int i=n;i>=1;i--)
+	{
+	   for(int s=n-i;s>=1;s--)
+	     {
+		System.out.print(" ");
+	     }
+      for(int j=1;j<=i;j++)
+	{
+	   System.out.print("* ");
+	}
+	   System.out.println();
+	}
+      for(int i=2;i<=n;i++)
+	{
+	   for(int s=1;s<=n-i;s++)
+	     {
+		System.out.print(" ");
+	     }
+      for(int j=1;j<=i;j++)
+	{
+	   System.out.print("* ");
+	}
+	   System.out.println();
+	}
+    }
+}
